1f3d9a1f6a5d2485a574a529f30aee4b9c5ffd8c
oncsr <khj20006@naver.com>
2019-12-14 17:18:07 +0900

    first commit : 입력 탭 ui 구현

---
+++ housekeeping_book.py
@@ -0,0 +1,75 @@
+import sys
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QBoxLayout
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QTabWidget
+from PyQt5.QtWidgets import QApplication
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import pyqtSlot
+
+class FirstTab(QWidget):
+	def __init__(self):
+		super(FirstTab, self).__init__()
+		self.init_widget()
+
+	def init_widget(self):
+		pb_1 = QPushButton()
+		pb_2 = QPushButton()
+
+		le_1 = QLineEdit()
+		le_2 = QLineEdit()
+
+		lb_1 = QLabel()
+		lb_2 = QLabel()
+
+		pb_1.setText("등록")
+		pb_2.setText("등록")
+
+		lb_1.setText("지출")
+		lb_2.setText("수입")
+		
+		layout_1 = QBoxLayout(QBoxLayout.LeftToRight, self)
+		layout_2 = QBoxLayout(QBoxLayout.TopToBottom)
+		layout_3 = QBoxLayout(QBoxLayout.TopToBottom)
+		
+		layout_2.addWidget(lb_1)
+		layout_2.addWidget(le_1)
+		layout_2.addWidget(lb_2)
+		layout_2.addWidget(le_2)
+		layout_3.addWidget(pb_1)
+		layout_3.addWidget(pb_2)
+
+		layout_1.addLayout(layout_2)
+		layout_1.addLayout(layout_3)
+
+		
+
+
+class Form(QWidget):
+	def __init__(self):
+		QWidget.__init__(self, flags=Qt.Widget)
+		self.setFixedWidth(640)
+		self.setFixedHeight(480)
+		self.tab = QTabWidget()
+		
+		self.init_widget()
+
+	def init_widget(self):
+		self.setWindowTitle("가계부")
+		
+		form_lbx = QBoxLayout(QBoxLayout.TopToBottom, parent=self)
+		self.setLayout(form_lbx)
+		form_lbx.addWidget(self.tab)
+
+		self.tab.addTab(FirstTab(), "입력")
+#self.tab.addTab(SecondTab(), "내역 확인")
+
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	form = Form()
+	form.show()
+	exit(app.exec_())
