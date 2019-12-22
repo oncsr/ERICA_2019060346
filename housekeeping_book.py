073be35762459713ccc839b4edaa7237c9d9ea67
oncsr <khj20006@naver.com>
2019-12-22 16:32:28 +0900

    ninth commit : 수입, 지출 파일로부터 내역을 불러와 TreeWidget에 보여주는 twadd 함수 추가(거의 완성)

--- housekeeping_book.py
+++ housekeeping_book.py
@@ -9,10 +9,8 @@ def CurrentTime():			#	현재 시간
 	real = str(cur.tm_year) + '/' + str(cur.tm_mon) + '/' + str(cur.tm_mday) + ' ' + str(cur.tm_hour) + ':' + str(cur.tm_mday) + ':' + str(cur.tm_sec)
 	return real
 
-def CurrentDay():			#	현재 날짜
-	cur = time.gmtime(time.time())
-	real = str(cur.tm_year) + '/' + str(cur.tm_mon) + '/' + str(cur.tm_mday)
-	return real
+def CurrentDay(fake):			#	현재 날짜
+	return fake.split(' ')[0]
 
 class FirstTab(QWidget):	#	가계부 탭
 	def __init__(self):
@@ -47,8 +45,14 @@ class FirstTab(QWidget):	#	가계부 탭
 		lb_5.setText("내용  ")
 		lb_6.setText("금액  ")
 		
-		blank_1 = QLabel()
-		blank_2 = QLabel()
+		self.tw_1 = QTreeWidget(self)
+		self.tw_1.setColumnCount(3)
+		self.tw_1.setHeaderLabels(["Date","Contents","Money"])
+		self.tw_2 = QTreeWidget(self)
+		self.tw_2.setColumnCount(3)
+		self.tw_2.setHeaderLabels(["Date","Contents","Money"])
+		self.root_1 = self.tw_1.invisibleRootItem()
+		self.root_2 = self.tw_2.invisibleRootItem()
 		
 		layout_1 = QBoxLayout(QBoxLayout.LeftToRight, self)
 		layout_2 = QBoxLayout(QBoxLayout.TopToBottom)
@@ -65,16 +69,44 @@ class FirstTab(QWidget):	#	가계부 탭
 		layout_2.addWidget(lb_6)
 		layout_2.addWidget(self.le_4)
 		
-		layout_3.addWidget(blank_1)
+		layout_3.addWidget(self.tw_1)
 		layout_3.addWidget(pb_1)
-		layout_3.addWidget(blank_2)
+		layout_3.addWidget(self.tw_2)
 		layout_3.addWidget(pb_2)
 		
 		layout_1.addLayout(layout_2)
 		layout_1.addLayout(layout_3)
 		
+		self.twadd()
+		
 		pb_1.clicked.connect(self.income)
+		pb_1.clicked.connect(self.twadd)
 		pb_2.clicked.connect(self.outcome)
+		pb_2.clicked.connect(self.twadd)
+
+	def twadd(self):
+		f = open("income.txt","r")
+		for i in f:
+			item = QTreeWidgetItem()
+			fdate, text, money = i.split(',')
+			date = CurrentDay(fdate)
+			item.setText(0, date)
+			item.setText(1, text)
+			item.setText(2, money)
+			self.root_1.addChild(item)
+		
+		f.close()
+		t = open("outcome.txt","r")
+		for j in t:
+			item = QTreeWidgetItem()
+			fdate, text, money = j.split(',')
+			date = CurrentDay(fdate)
+			item.setText(0, date)
+			item.setText(1, text)
+			item.setText(2, money)
+			self.root_2.addChild(item)
+
+		t.close()
 
 	def income(self):
 		f = open("income.txt","a")
